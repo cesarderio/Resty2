9d69b8fe4a3c1a9e05c8427a8e0e141ab88bfe17
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-05 18:21:10 -0800

    reworking on second resty application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+// import { useState, useEffect } from 'react';
 
-import './app.scss';
+import axios from "axios";
+
+import "./app.scss";
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Form from "./components/form";
+import Results from "./components/results";
 
+const App = () => {
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
+  const [method, setMethod] = useState("get");
+  const [loading, setLoading] = useState(false);
 
-// const App = () => {
+  const apiCallOut = (requestParams) => {
+    setLoading(true);
+    setRequestParams(requestParams);
+  };
 
-//   let callApi = (requestParams) => {
+  useEffect(() => {
+    const callApi = async (requestParams) => {
+      console.log(requestParams);
+
+      let newData = await axios[method](requestParams.url);
+      setData(newData.data);
+      setLoading(false);
+      setMethod(requestParams.method);
+    };
+
+    if (Object.keys(requestParams).length > 0) {
+      callApi(requestParams);
+    }
+    console.log(requestParams);
+  }, [requestParams]);
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <Form handleApiCall={apiCallOut} data={setData} method={method} />
+      <Results data={data} loading={loading} />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
+//   callApi = (requestParams) => {
 //     // mock output
 //     const data = {
 //       count: 2,
@@ -23,6 +71,7 @@ import Results from './components/results';
 //     };
 //     this.setState({data, requestParams});
 //   }
+//   render() {
 //     return (
 //       <React.Fragment>
 //         <Header />
@@ -33,42 +82,7 @@ import Results from './components/results';
 //         <Footer />
 //       </React.Fragment>
 //     );
+//   }
 // }
-
-
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    this.setState({data, requestParams});
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
 
 export default App;
